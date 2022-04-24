005da891f5b221cdb4f3bb553e6a9876325eca82
주용준 <dltlaos14@naver.com>
2022-04-24 20:25:39 +0900

    백준 1316 풀이

--- 1316.py
+++ 1316.py
@@ -1,11 +1,12 @@
-num = int(input())
+import sys
+num = int(sys.stdin.readline())
 result = num
-for i in range(0,num):
-    word=input()
-    for j in range(0,len(word)-1):
-        if word[j]==word[j+1]:
+for i in range(num):
+    word = sys.stdin.readline()
+    for j in range(len(word)-1):
+        if word[j] == word[j+1]:
             pass
-        elif word[j] in word[j+1:]:
+        elif word[j] in word[j+2:]:
             result-=1
             break
 print(result)
