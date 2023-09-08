8f4ec8afde6b8f21b4f8a8840a04453c8c5a12b5
dltlaos11 <dltlaos14@naver.com>
2023-09-08 21:10:54 +0900

    boj permutations & combinations

--- BOJ/15649.py
+++ BOJ/15649.py
@@ -1,17 +1,26 @@
-N, M = map(int, input().split())
-visited = [False] * N
-res = []
+# N, M = map(int, input().split())
+# visited = [False] * N
+# res = []
+#
+# def solve(depth, N, M):
+#     if depth == M:
+#         print(' '.join(map(str, res)))
+#         return
+#     for i in range(len(visited)):
+#         if not visited[i]:
+#             visited[i] = True
+#             res.append(i+1)
+#             solve(depth+1, N, M)
+#             visited[i] = False
+#             res.pop()
+#
+# solve(0, N, M)
+from itertools import permutations
 
-def solve(depth, N, M):
-    if depth == M: 
-        print(' '.join(map(str, res)))
-        return
-    for i in range(len(visited)):
-        if not visited[i]:
-            visited[i] = True
-            res.append(i+1)
-            solve(depth+1, N, M)
-            visited[i] = False
-            res.pop()
-
-solve(0, N, M)
+n, m = map(int,input().split())
+arr = [i for i in range(1, n+1)]
+res = list(permutations(arr, m))
+for i in res:
+    for j in i:
+        print(j, end=' ')
+    print()
--- BOJ/15650.py
+++ BOJ/15650.py
@@ -1,16 +1,26 @@
-N, M = map(int, input().split())
-visited = [False] * N
-res = []
+# N, M = map(int, input().split())
+# visited = [False] * N
+# res = []
+#
+# def solve(depth, idx, N, M):
+#     if depth == M:
+#         print(' '.join(map(str, res)))
+#         return
+#     for i in range(idx, N):
+#         if not visited[i]:
+#             visited[i] = True
+#             res.append(i+1)
+#             solve(depth+1, i+1, N, M)
+#             visited[i] = False
+#             res.pop()
+# solve(0, 0, N, M)
 
-def solve(depth, idx, N, M):
-    if depth == M:
-        print(' '.join(map(str, res)))
-        return
-    for i in range(idx, N):
-        if not visited[i]:
-            visited[i] = True
-            res.append(i+1)
-            solve(depth+1, i+1, N, M)
-            visited[i] = False
-            res.pop()
-solve(0, 0, N, M)
+from itertools import combinations
+
+n, m = map(int,input().split())
+arr = [i for i in range(1, n+1)]
+res = list(combinations(arr, m))
+for i in res:
+    for j in i:
+        print(j, end=' ')
+    print()
