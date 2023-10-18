68dd760007a23a8b0c85d230e8c0364b6ceed308
dltlaos11 <dltlaos14@naver.com>
2023-10-18 20:09:29 +0900

    boj 11057 dp

---
+++ BOJ/dp/11057.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+dp = [1]*10 # 맨 뒷자리 수 가 0일 떄 는 모든 수의 길이가 1 (0)
+for i in range(1,n) : # 수의 길이 1 ~9
+    for j in range(1,10) : # 맨 뒷자리
+
+        dp[j] += dp[j-1]
+        # print(dp[j], dp[0]) # dp가 3일 떄 기존의 dp[1,1,1, ... 1,1] 2번 실행
+
+print(sum(dp)%10007)
