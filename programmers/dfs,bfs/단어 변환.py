6b42e8a1c346cbd8bebbf322e234c11360e4cc21
dltlaos11 <dltlaos14@naver.com>
2023-12-18 17:26:40 +0900

    edit file

--- programmers/dfs,bfs/단어 변환.py
+++ programmers/dfs,bfs/단어 변환.py
@@ -3,7 +3,7 @@ from collections import deque
 def solution(begin, target, words):
     answer = 0
     queue = deque()
-    visited = [False] * len(words)    # 방문 노드 여부 확인 리스트
+    visited = [False] * len(words)    
     
     def bfs(wd, depth):
         nonlocal answer
@@ -19,12 +19,16 @@ def solution(begin, target, words):
             for i in range(len(words)):
                 temp = 0
                 if not visited[i]:
+                    # 그 단어가 words 속 단어와 다를 때 한 자씩 비교해서 더하기
                     for j in range(len(word)):
                         if word[j] != words[i][j]:
+                            print(word, word[j], words[i][j], j)
                             temp += 1
 
                     if temp == 1:   # 만약 다른 글자 개수가 1개라면
                         queue.append([words[i], cnt+1])
+                        print(queue)
                         visited[i] = True
+                        print(visited)
     bfs(begin, 0)
     return answer
