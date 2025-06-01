5c372f80140386ea5b87811a1ac189bfd7f15451
YONGJUN JU <dltlaos14@naver.com>
2025-06-02 00:26:15 +0900

    [Silver III] Title: 칸토어 집합, Time: 92 ms, Memory: 110776 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,12 +1,12 @@
-def func(k):
-	if k == 0:
-		return '-'
+ans = ['' for _ in range(13)]
+ans[0] = '-'
 
-	return func(k-1) + (' ' * (3 ** (k-1))) + func(k-1)
+for i in range(1, 13):
+	ans[i] = ans[i-1] + (' ' * (3 ** (i-1))) + ans[i-1]
 
 while True:
 	try:
 		N = int(input())
-		print(func(N))
+		print(ans[N])
 	except:
 		break
