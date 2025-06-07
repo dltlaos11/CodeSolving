1c29a5fd4d1f8d9e10c415f50b59ee3ef1fb8441
YONGJUN JU <dltlaos14@naver.com>
2025-06-07 17:15:54 +0900

    [Silver II] Title: 로또, Time: 96 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,26 +1,15 @@
-def combination(index, level):
-
-	if level == R:
-		for c in choose:
-			print(c, end=' ')
-		print()
-		return
-
-	for i in range(index, N):
-		choose.append(lst[i])
-		combination(i+1, level+1)
-		choose.pop()
+from itertools import combinations
 
 while True:
-	try:
-		lst = list(map(int, input().split()))
-		N = lst[0]
-		R = 6
-		lst = lst[1:]
-		choose = []
+	I = list(map(int, input().split()))
 
-		combination(0,0)
-		print()
-
-	except:
+	k = I[0]
+	arr = I[1:]
+	if k == 0:
 		break
+
+	for comb in combinations(arr, 6):
+		for u in comb:
+			print(u, end=' ')
+		print()
+	print()
