874dafb087c504fe40c17e8b3c957fa1ab5a86c4
YONGJUN JU <dltlaos14@naver.com>
2025-06-15 23:08:23 +0900

    [level 2] Title: 피로도, Time: 122.04 ms, Memory: 9.68 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,15 +1,36 @@
-from itertools import permutations
 def solution(k, dungeons):
+    answer = []
+    R = len(dungeons)
+    check = [False for i in range(R)]
     temp = []
-    for hardness in list(permutations(dungeons)):
-        charge = k
-        cnt = 0
-        for hard in hardness:
-            if charge >= hard[0]:
-                charge -= hard[1]
-                cnt +=1
-            else:
-                break
-        temp.append(cnt)
-    answer = max(temp)
+    
+    def permutation(level):
+        
+        if level == R:
+            
+            charge = k
+            cnt = 0
+            for hard in temp:
+                if charge >= hard[0]:
+                    charge -= hard[1]
+                    cnt +=1
+                else:    
+                    break
+            answer.append(cnt)
+            return
+        
+        for i in range(len(dungeons)):
+            
+            if check[i] == True:
+                continue
+            
+            check[i] = True
+            temp.append(dungeons[i])
+            permutation(level+1)
+            temp.pop()
+            check[i] = False
+
+    permutation(0)
+    answer = max(answer)
+    
     return answer
