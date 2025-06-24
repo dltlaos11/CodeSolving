a2132fa3a27553f24003e97089e33980c96236d8
YONGJUN JU <dltlaos14@naver.com>
2025-06-25 00:38:09 +0900

    [level 2] Title: 광물 캐기, Time: 81.77 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,60 +1,33 @@
-import sys
-
 def solution(picks, minerals):
-    answer = sys.maxsize
+    ans = []
+    tired = [[1, 1, 1],
+             [5, 1, 1],
+             [25, 5, 1]]
+    idx = {"diamond": 0, "iron": 1, "stone": 2}
     
-    # 피로도 테이블: tired_list[곡괭이타입][광물타입]
-    tired_list = [
-        [1, 1, 1],    # 다이아몬드 곡괭이
-        [5, 1, 1],    # 철 곡괭이  
-        [25, 5, 1]    # 돌 곡괭이
-    ]
-    
-    # 광물 타입을 인덱스로 매핑
-    mineral_to_idx = {"diamond": 0, "iron": 1, "stone": 2}
-    
-    visited = [0] * 3  # 각 곡괭이 타입별 사용 개수
-    orders = []        # 곡괭이 사용 순서
-    
-    def calc(depth):
-        """depth번째 곡괭이로 광물을 캤을 때의 피로도 계산"""
-        plus = 0
-        pick_type = orders[depth]
+    def dfs(picks, minerals, tired_rate):
         
-        # depth번째 곡괭이로 캘 수 있는 광물 범위: [depth*5, (depth+1)*5)
-        for i in range(depth * 5, min((depth + 1) * 5, len(minerals))):
-            mineral_type = mineral_to_idx[minerals[i]]
-            plus += tired_list[pick_type][mineral_type]
-        
-        return plus
-    
-    def dfs(depth, tired):
-        nonlocal answer
-        
-        # 모든 곡괭이를 사용했거나 모든 광물을 캤으면 종료
-        if depth == sum(picks) or depth * 5 >= len(minerals):
-            answer = min(answer, tired)
+        if sum(picks) == 0 or minerals == []:
+            ans.append(tired_rate)
             return
         
-        # 가지치기: 현재 피로도가 이미 최소값보다 크면 중단
-        if tired >= answer:
-            return
+        tmp = minerals[:5]
         
-        # 각 곡괭이 타입 시도
-        for pick_type in range(3):
-            if visited[pick_type] < picks[pick_type]:
-                visited[pick_type] += 1
-                orders.append(pick_type)
-                
-                plus = calc(depth)
-                
-                # 가지치기: 미리 체크
-                if tired + plus < answer:
-                    dfs(depth + 1, tired + plus)
-                
-                # 백트래킹
-                visited[pick_type] -= 1
-                orders.pop()
+        for i in range(3):
+            if picks[i] > 0:
+                picks[i] -= 1
+                tmp_tired_rate = calc_tired_rate(i, tmp)
+                dfs(picks, minerals[5:], tmp_tired_rate+tired_rate)
+                picks[i] += 1
+            
+    def calc_tired_rate(i, minerals):
+        tired_rate = 0
+        
+        for m in minerals:
+            tired_rate += tired[i][idx[m]]
+        return tired_rate
     
-    dfs(0, 0)
-    return answer
+    dfs(picks, minerals, 0)
+    ans.sort()
+    
+    return ans[0]
